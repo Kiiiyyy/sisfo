43e9a54f5eea9a85f78fde9340df2038d71dc20f
Kiiiyyy <dzakyramadhani14510@gmail.com>
2025-08-05 13:29:26 +0700

    cek

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nim')->unique();
-            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('pasword');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timesemailtamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
